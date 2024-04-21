8c75e3e044a56c7f790fed1023ee04c089c8a29e
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-04-21 15:17:56 +0530

    Update DB.js

--- DB.js
+++ DB.js
@@ -1,6 +1,6 @@
 const mongoose= require ('mongoose');
 const dotenv = require('dotenv');
-const DB_URI = dotenv.config().parsed.DB_URI;
+const DB_URI = process.env.DB_URI;
 
 module.exports={
     DB: DB_URI
